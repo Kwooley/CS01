a4b1bcc9fb313a757d4ca9c140beaf2f598f9837
Kayw Lee <kwooler@gmail.com>
2020-01-25 14:50:51 -0800

    challenge 1

--- ch02/example.cpp
+++ ch02/example.cpp
@@ -1,10 +1,25 @@
 #include	<iostream>
-#include	<iomanip>
 
 using namespace std;
 
 int main()
 {
-	int	i;
+	double	tax = 0.0675;
+	double	tip = 0.2;
+	double	taxAmount,
+		tipAmount,
+		totalBill;
+
+	// git stage changed
+	double 	mealCharge = 88.67;
+	
+	taxAmount = mealCharge * tax;
+	tipAmount = (mealCharge + taxAmount) * 0.2;
+
+	totalBill = mealCharge + taxAmount + tipAmount;
+
+	cout << "Tax amount " << taxAmount << endl;
+	cout << "Tip amount " << tipAmount << endl;
+	cout << "Total Bill is " << totalBill << endl;
 
 } 
