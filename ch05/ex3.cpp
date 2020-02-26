a710196dac4638581a2e8a918d0fb4f605d554ec
Kayw Lee <kwooler@gmail.com>
2020-02-26 09:29:16 -0800

    Feb 26

--- ch05/ex3.cpp
+++ ch05/ex3.cpp
@@ -6,26 +6,14 @@ using   namespace       std;
 
 int main()
 {
-        //* Input : two integers [from .. end]
-        //* Requirements for the input
-        //?    [0..100]   > 0 , < 100
-        //?    from < end
-        //* Print out all even numbers 
-        //*             in the given user range
-        int     from, end, i;
-        cout << "Enter the range\n";
-        cin   >> from >> end;
-        while ( (from < 0) || (end >100) || (from >=end))
-        {
-                cout << "Enter the range\n";
-                cin   >> from >> end;
-        }
-        i = from; 
-        while ( i < end)
-        {
-               if ( !(i % 2) )  
-                        cout << i << endl;
-                i++;
-        }
+      int       i;
 
+      i = 10;
+      do{
+              cout << "value i " << i;
+      } while(i++ <10);
+
+      i = 10;
+      while ( i < 10)
+                cout << "value i " << i;
 }
