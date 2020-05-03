bf8c6f0db94cba633884b200c51bef5b725f64fc
Kayw Lee <kwooler@gmail.com>
2020-05-02 20:53:55 -0700

    10-6

---
+++ ch10/cooplab10-6prob.cpp
@@ -0,0 +1,41 @@
+#include    <iostream>
+#include    <fstream>
+using namespace std;
+
+const   unsigned short MAX=5000;
+string  stname[MAX];
+char    gender[MAX];
+int     year[MAX];
+string  name[MAX];
+int     cnt[MAX];
+int     NUMRECORD;
+
+int     constructRecord(void);
+void    retrieveRecord(int, string);
+void    printRecord(int);
+int main()
+{
+        int     year;
+        string state;
+
+        NUMRECORD = constructRecord();
+        for(int i=0;i<NUMRECORD; i++)
+            printRecord(i);
+
+        cout << "Enter the year: (2011-2018)";
+        cin >> year;
+        cout << "Enter the stname (2 upper case letters): ";
+        cin >> state;
+        retrieveRecord(year, state);
+}
+int     constructRecord()
+{
+}
+void    retrieveRecord(int useryear, string userstate)
+{
+}
+void    printRecord(int idx)
+{
+        cout <<  stname[idx] << "\t\t" << gender[idx] << "\t" << year[idx] << "\t" << 
+                setw(10) << name[idx] << "\t" << cnt[idx] << "\n" ;
+}
