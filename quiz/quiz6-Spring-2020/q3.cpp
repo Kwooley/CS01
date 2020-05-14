0391d7bb06966ccfc6ff625c55c65e8c7b8bbacf
Kayw Lee <kwooler@gmail.com>
2020-05-14 16:50:57 -0700

    Q6

---
+++ quiz/quiz6-Spring-2020/q3.cpp
@@ -0,0 +1,17 @@
+#include    <iostream>
+#include    <string>
+using namespace std;
+
+int main()
+{
+    string str1="C++ Programming";
+    string::reverse_iterator rit;
+
+    for(int i=0;i<str1.length(); i++)
+        cout << str1[i];
+    cout << endl;
+    for(rit=str1.rbegin(); rit != str1.rend(); rit++)
+        cout << *rit ;
+    cout << endl;
+
+}
