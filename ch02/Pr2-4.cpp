2e61e425c7b7d6e925f9f8fd3fc67efbcabbda8f
Kayw Lee <kwooler@gmail.com>
2020-08-24 18:08:55 -0700

    Ch02

---
+++ ch02/Pr2-4.cpp
@@ -0,0 +1,15 @@
+// An unruly printing program
+#include <iostream>
+#include	<climits>
+using namespace std;
+
+int main()
+{
+   cout << "The following items were top sellers";
+   cout << "during the month of June:";
+   cout << "Computer games";
+   cout << "Coffee";
+   cout << "Aspirin";
+   count << "max value for int", int_max;
+   return 0;
+} 
