e8f089766ef0729741b9332fb53fe2f2882b5e29
Kwooley <kwooler@gmail.com>
2020-10-07 08:03:07 -0700

    ch 06 examples

---
+++ ch06/exercise6.10.2.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int     localmain=10;
+    int     i;
+    int     j;
+    
+    i = 10;
+    j = 30;
+
+    if ( 1 ){
+        int localif=100;
+        int i;
+        i = 20;
+        cout << "The value i " << i << endl;
+        cout << "The value j of the main functin " << j << endl;
+    }
+
+    cout << "The value i " << i << endl; //* The value of variable ?
+    //* Can we access the variable "localif" in the if block? Why? 
+}
