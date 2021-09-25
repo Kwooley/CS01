1f35de45dd79201ebcc32712211ff2657cfa8958
Kayw Lee <kwooler@gmail.com>
2021-09-25 12:00:40 -0700

    chapter 13

---
+++ ch13/rect1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+
+class Rectangle {
+	private:
+		int width;
+		int height;
+	public:
+		int 	getWidth() const { return width; };
+		int 	getHeight() const { return height; };
+		int 	getArea() const { return width*height;} ;
+		void 	setWidth(int w) { width = w;};
+		void 	setHeight(int h) { height = h;};
+};
+
+
+int main()
+{
+	Rectangle r1;
+	r1.setWidth(10);
+	r1.setHeight(20);
+
+	cout << "Width " << r1.getWidth() << endl;
+	cout << "Height " << r1.getHeight() << endl;
+	cout << "area " << r1.getArea() << endl;
+
+}
