755372fb7db117597a60cffabc977708c5b13cad
Kayw Lee <kwooler@gmail.com>
2021-10-09 23:38:57 -0700

    Chapter 14

--- ch14/copyconstructor2.cpp
+++ ch14/copyconstructor2.cpp
@@ -23,6 +23,14 @@ public:
 		for (int l = 0; l < NUM_SCORES; l++)
 			scores[l] = sc[l];
 	}
+	~Student()
+	{
+		cout << "\t\t\tDestructor is working \t";
+		for (int i = 0; i < NUM_SCORES; i++)
+			cout << fixed << setprecision(2) << scores[i] << "\t";
+		delete[] scores;
+		cout << endl;
+	}
 	int getSid() const { return sid; }
 	string getSname() const { return sname; }
 	double *getScores() const { return scores; }
@@ -34,25 +42,29 @@ public:
 			scores[i] = s[i];
 	}
 };
-
+void printStudent(Student s)
+{
+	double *sc;
+	cout << "ID: " << s.getSid() << "\t";
+	cout << "Name: " << s.getSname() << "\t";
+	sc = s.getScores();
+	for (int i = 0; i < NUM_SCORES; i++)
+		cout << fixed << setprecision(2) << sc[i] << "\t";
+	cout << endl;
+}
 int main()
 {
 	double sc1[NUM_SCORES] = {99.10, 98.72, 89.71};
 	Student s1(1001, "John", sc1);
+	printStudent(s1);
+
+	double sc2[NUM_SCORES] = {100.00, 99.99, 88.88};
+	s1.setScores(sc2);
+	printStudent(s1); // Error
 
 	Student s2(s1);
-	double *sc = s2.getScores();
-	cout << "S2's scores\n";
-	for (int i = 0; i < NUM_SCORES; i++)
-		cout << fixed << setprecision(2) << sc[i] << "\t";
-	cout << endl;
-
-	double sc2[NUM_SCORES] = {0.0, 0.0, 0.0};
-	s2.setScores(sc2);
-	sc = s1.getScores();
-
-	cout << "After updating the s2's score, S1's scores\n";
-	for (int i = 0; i < NUM_SCORES; i++)
-		cout << fixed << setprecision(2) << sc[i] << "\t";
-	cout << endl;
+	printStudent(s2);
+	double sc3[NUM_SCORES] = {0.0, 0.0, 0.0};
+	s2.setScores(sc3);
+	printStudent(s1);
 }
