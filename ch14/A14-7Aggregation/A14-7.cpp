900b47d95ddc9cdd3e1eee1da92737d7aaec01bd
Kayw Lee <kwooler@gmail.com>
2021-10-12 16:55:36 -0700

    A14-7

---
+++ ch14/A14-7Aggregation/A14-7.cpp
@@ -0,0 +1,27 @@
+#include "DOB.hpp"
+#include "Course.hpp"
+#include "Student.hpp"
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+	const int N = 5;
+	Student s[N];
+	ifstream ifs;
+
+	ifs.open("studentcourses.txt");
+	if (ifs.fail())
+	{
+		cerr << "File open error\n";
+		exit(0);
+	}
+
+	for (int i = 0; i < N; i++)
+	{
+		ifs >> s[i];
+		cout << s[i] << endl;
+	}
+}
