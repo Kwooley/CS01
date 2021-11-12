f5712c73db84b0bba12dc9074964821b3838cda2
Kayw Lee <kwooler@gmail.com>
2021-11-11 23:31:56 -0800

    Polymorphism

---
+++ ch15/Polymorphism1.cpp
@@ -0,0 +1,28 @@
+
+#include <iostream>
+using namespace std;
+
+class base
+{
+public:
+    void print()
+    {
+        cout << "Base Class print function\n";
+    }
+};
+class derived : public base
+{
+public:
+    void print()
+    {
+        cout << "Derived Class print function\n";
+    }
+};
+
+int main()
+{
+    base *baseptr;
+    derived d;
+    baseptr = &d;
+    baseptr->print();
+}
