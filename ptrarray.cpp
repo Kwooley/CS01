187ade67ae6e0ae7734ca5e9bd301d25248f8641
Kyu Lee <kwooler@gmail.com>
2022-03-14 12:16:34 -0700

    ptr

--- ptrarray.cpp
+++ ptrarray.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 using namespace std;
 
-void printarrays(int *, int);
+void printArrays(int *, int);
 int main()
 {
 	const int N = 5;
 	int numbers[] = {10, 20, 30, 40, 50};
 
-	printarrays(numbers, N);
+	printArrays(numbers, N);
 }
 
-void printarrays(int *arr, int len)
+void printArrays(int *arr, int len)
 {
 	for (int i = 0; i < len; i++)
 	{
