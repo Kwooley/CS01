bb3ca376aff6f195f974a63a45e9e3484fb66117
Kyu Lee <kwooler@gmail.com>
2022-09-23 19:48:51 -0700

    A6

--- ch04/Lab4-3.cpp
+++ ch04/Lab4-3.cpp
@@ -9,52 +9,52 @@
 //*     Example
 //*        Package weight = 15 kg, Distance = 650 miles
 //*             Shipping Price = (distance / 500.0) * rate;
-//*                                = 650 / 500.0 * 4.80 
+//*                                = 650 / 500.0 * 4.80
 
-//*     Write a program that asks for the weight of the package and 
+//*     Write a program that asks for the weight of the package and
 //*     the distance it is to beshipped, and then displays the charges.
-//** Input Validation : 
-//* Do not accept values of 0 or less for the weight of the package. 
-//* Do not accept weights of more than 20 kg (this is the maximum weight the company will ship). 
-//* Do not accept distances of less than 10 miles or more than 3,000 miles. 
-//*     These are the company’s minimum and maximum shipping distances. 
+//** Input Validation :
+//* Do not accept values of 0 or less for the weight of the package.
+//* Do not accept weights of more than 20 kg (this is the maximum weight the company will ship).
+//* Do not accept distances of less than 10 miles or more than 3,000 miles.
+//*     These are the company’s minimum and maximum shipping distances.
 
-#include        <iostream>
-using   namespace std; 
-int     main()
+#include <iostream>
+using namespace std;
+int main()
 {
         const double RATE2 = 1.10;
         const double RATE6 = 2.20;
         const double RATE10 = 3.70;
         const double RATE20 = 4.80;
-        double  weight, distance, dRate, price;
+        double weight, distance, dRate, price;
 
         cout << "Enter the package weight and distance\n";
-        cin >> weight >> distance ;
+        cin >> weight >> distance;
         //? Input Validation
-        if (( weight < 0 ) && (weight > 20))
+        if ((weight < 0) && (weight > 20))
         {
                 cout << "The package weight must be a positive number and less than 20.\n";
                 exit(0);
         }
 
-        if ( weight < 2)
+        if (weight < 2)
                 dRate = RATE2;
-        else if ( weight < 6)
+        else if (weight < 6)
                 dRate = RATE6;
-        else if ( weight < 10)
+        else if (weight < 10)
                 dRate = RATE10;
-        else if ( weight <= 20)
+        else if (weight <= 20)
                 dRate = RATE20;
-        else {
+        else
+        {
                 cout << "The weight must be less than 20\n";
                 exit(0);
         }
-        
-        if ( distance < 500)
+
+        if (distance < 500)
                 price = dRate;
-        else 
+        else
                 price = (distance / 500.0) * dRate;
         cout << "The shipping price for package is " << price << endl;
-
 }
--- ch05/lab5-11.cpp
+++ ch05/lab5-11.cpp
@@ -1,32 +1,31 @@
 //	Lab 5-11 : Problem Definition
-//	Make a program that find the prime numbers in the given range. 
+//	Make a program that find the prime numbers in the given range.
 //		example) find the prime numbers in the range [1..100]
 //		2, 3, 5, 7, ... , 97
 
-#include	<iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
-	int 	begin, end;
-	int	i, p;
+	int begin, end;
+	int i, p;
 
-	do {
+	do
+	{
 		cout << "Enter your range [from, to] \n";
 		cin >> begin >> end;
-	} while( begin >= end || begin < 2 || end < 0) ;
+	} while (begin >= end || begin < 2 || end < 0);
 
-
-	for(p=begin; p<=end; p++)
+	for (p = begin; p <= end; p++)
 	{
-		if ( p ==0 || p == 1)
+		if (p == 0 || p == 1)
 			continue;
 		//* for(i=2; i<p; i++)
-		for(i=2; i<=(p/2); i++)
-			if ( p % i == 0) 
+		for (i = 2; i <= (p / 2); i++)
+			if (p % i == 0)
 				break;
-		if ( i > (p/2) )
+		if (i > (p / 2))
 			cout << " Prime number : " << p << endl;
 	}
-
 }
