399c7e142582601f23b7b98b9b013be6332ac8f3
Kyu Lee <kwooler@gmail.com>
2022-10-08 00:03:11 -0700

    ch07

--- ch07/cooplab7-8prob.cpp
+++ ch07/cooplab7-8prob.cpp
@@ -1,26 +1,28 @@
-#include		<iostream>
-#include		<fstream>
-#include 		<string>
-using	namespace std;
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
 
-int 		makeNameRecord(string [], string [], int [], string [], int []);
-void		printoutallrecords(int, string [], string [], int [], string [], int []);
-void		printnamewithA(int, string [], string [], int [], string [], int []);
-void	    printoutcontents(string , string , int , string , int );
+int makeNameRecord(string[], string[], int[], string[], int[]);
+void printoutallrecords(int, string[], string[], int[], string[], int[]);
+void findNames(int, string[], string[], int[], string[], int[], char, string);
+void printoutcontents(string, string, int, string, int);
 
-const int N=5000;
-int		main()
+const int N = 5000;
+int main()
 {
-	string	state[N];
-	string	gender[N];
-	int		year[N];
-	string	name[N];
-	int		count[N];
-	int 	cnt;
+	string state[N];
+	string gender[N];
+	int year[N];
+	string name[N];
+	int count[N];
+	int cnt;
+	char starting;
+	string stname;
+	starting = 'S';
+	stname = "CA";
 
 	cnt = makeNameRecord(state, gender, year, name, count);
 	printoutallrecords(cnt, state, gender, year, name, count);
-	printnamewithA(cnt, state, gender, year, name, count);
-
+	findNames(cnt, state, gender, year, name, count, starting, stname);
 }
-
