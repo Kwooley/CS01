c847ae22ec8da04c2a04853c90f798b2ef5008d0
Kayw Lee <kwooler@gmail.com>
2023-11-23 12:56:29 -0800

    B1

--- ch08/BubbleSort1.cpp
+++ ch08/BubbleSort1.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-void bubbleSort(int [], int );
+void bubbleSort(int[], int);
 
 int main()
 {
@@ -9,20 +9,19 @@ int main()
 	int array[N] = {25, 10, 15, 30, 35, 40, 45, 55, 20, 50};
 
 	bubbleSort(array, N);
-	for ( int v: array)
+	for (int v : array)
 		cout << v << "\t";
 	cout << endl;
 }
 
 void bubbleSort(int array[], int N)
 {
-	int min, index;
-	for(int i=0; i < N-1; i++)
+	for (int i = 0; i < N - 1; i++)
 	{
-		for(int j=0; j<N-i-1; j++)
+		for (int j = 0; j < N - i - 1; j++)
 		{
-			if ( array[j] > array[j+1])
-				swap(array[j], array[j+1]);
+			if (array[j] > array[j + 1])
+				swap(array[j], array[j + 1]);
 		}
 		// for(int i=0;i<N;i++)
 		// 	cout << array[i] << "\t";
